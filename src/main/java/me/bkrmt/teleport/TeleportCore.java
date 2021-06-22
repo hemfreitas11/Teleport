a4d65f0559559b0a94d6aeb25fcb23784c11d97f
hemfreitas11 <hemfreitas11@users.noreply.github.com>
2021-06-22 15:58:50 -0300

    Leak fix

--- src/main/java/me/bkrmt/teleport/TeleportCore.java
+++ src/main/java/me/bkrmt/teleport/TeleportCore.java
@@ -12,59 +12,16 @@ import org.bukkit.scheduler.BukkitTask;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-public enum TeleportCore {
+public enum TeleportCore implements Listener {
     INSTANCE;
 
-    private static Dictionary<String, CancelCause> cancelCause = new Hashtable<>();
-    private static Dictionary<String, Boolean> playersInCooldown = new Hashtable<>();
-    private static Dictionary<String, BukkitTask> playerTeleport = new Hashtable<>();
-    private static Dictionary<String, Object[]> invulnerablePlayers = new Hashtable<>();
+    private static final Dictionary<String, CancelCause> cancelCause = new Hashtable<>();
+    private static final Dictionary<String, Boolean> playersInCooldown = new Hashtable<>();
+    private static final Dictionary<String, BukkitTask> playerTeleport = new Hashtable<>();
+    private static final Dictionary<String, Object[]> invulnerablePlayers = new Hashtable<>();
 
     public void start(BkPlugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler
-            public void onPlayerTakeDamage(EntityDamageEvent event) {
-                if (event.getEntityType().equals(EntityType.PLAYER)) {
-                    Player damagedPlayer = (Player) event.getEntity();
-
-                    if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagedPlayer.getName()) != null) {
-                        Object[] values = TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagedPlayer.getName());
-
-                        int length = (int) values[0];
-                        float count = (float) values[1];
-
-                        float remaining = (length - count);
-                        if (remaining > 0) {
-                            event.setCancelled(true);
-                            return;
-                        }
-                    }
-
-                    if (TeleportCore.INSTANCE.getPlayersInCooldown().get(damagedPlayer.getName()) != null) {
-                        TeleportCore.INSTANCE.getPlayersInCooldown().put(damagedPlayer.getName(), false);
-                        TeleportCore.INSTANCE.getCancelCause().put(damagedPlayer.getName(), CancelCause.DealtDamage);
-                    }
-                }
-            }
-
-            @EventHandler
-            public void onPlayerDealDamage(EntityDamageByEntityEvent event) {
-                if (event.getDamager().getType().equals(EntityType.PLAYER)) {
-                    Player damagerPlayer = (Player) event.getDamager();
-
-                    if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagerPlayer.getName()) != null) {
-                        if (checkInvulnerable(event, damagerPlayer, 0, damagerPlayer.getName())) return;
-                    } else if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(event.getEntity().getName()) != null) {
-                        if (checkInvulnerable(event, damagerPlayer, 1, event.getEntity().getName())) return;
-                    }
-
-                    if (TeleportCore.INSTANCE.getPlayersInCooldown().get(damagerPlayer.getName()) != null) {
-                        TeleportCore.INSTANCE.getPlayersInCooldown().put(damagerPlayer.getName(), false);
-                        TeleportCore.INSTANCE.getCancelCause().put(damagerPlayer.getName(), CancelCause.TookDamage);
-                    }
-                }
-            }
-        }, plugin);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
         TeleportCore.INSTANCE.getPlayersInCooldown().put("Core-Started", true);
     }
 
@@ -102,5 +59,48 @@ public enum TeleportCore {
             }
         }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerTakeDamage(EntityDamageEvent event) {
+        if (event.getEntityType().equals(EntityType.PLAYER)) {
+            Player damagedPlayer = (Player) event.getEntity();
+
+            if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagedPlayer.getName()) != null) {
+                Object[] values = TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagedPlayer.getName());
+
+                int length = (int) values[0];
+                float count = (float) values[1];
+
+                float remaining = (length - count);
+                if (remaining > 0) {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+
+            if (TeleportCore.INSTANCE.getPlayersInCooldown().get(damagedPlayer.getName()) != null) {
+                TeleportCore.INSTANCE.getPlayersInCooldown().put(damagedPlayer.getName(), false);
+                TeleportCore.INSTANCE.getCancelCause().put(damagedPlayer.getName(), CancelCause.DealtDamage);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDealDamage(EntityDamageByEntityEvent event) {
+        if (event.getDamager().getType().equals(EntityType.PLAYER)) {
+            Player damagerPlayer = (Player) event.getDamager();
+
+            if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(damagerPlayer.getName()) != null) {
+                if (checkInvulnerable(event, damagerPlayer, 0, damagerPlayer.getName())) return;
+            } else if (TeleportCore.INSTANCE.getInvulnerablePlayers().get(event.getEntity().getName()) != null) {
+                if (checkInvulnerable(event, damagerPlayer, 1, event.getEntity().getName())) return;
+            }
+
+            if (TeleportCore.INSTANCE.getPlayersInCooldown().get(damagerPlayer.getName()) != null) {
+                TeleportCore.INSTANCE.getPlayersInCooldown().put(damagerPlayer.getName(), false);
+                TeleportCore.INSTANCE.getCancelCause().put(damagerPlayer.getName(), CancelCause.TookDamage);
+            }
+        }
     }
 }
